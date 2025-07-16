e0f6f925bde6f5c78eabffae3e3c9aa5cdbb86a5
nexeralt <playerok1artik@gmail.com>
2025-07-16 19:53:21 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,2 +1,2 @@
-print("Hello, this shit worked!")
+print("Hello, this shit worked lolz!")
 -- loadstring(game:HttpGet("https://raw.githubusercontent.com/nexeralt/Die-Of-Death/refs/heads/main/main.lua"))()
