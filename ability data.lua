3268dc84e3f621d6cd92475a640c6d1e7d312b3f
nexeralt <playerok1artik@gmail.com>
2025-07-27 15:42:35 +0900

    Update ability data.lua

--- ability data.lua
+++ ability data.lua
@@ -11,6 +11,7 @@ return {
 ["Hotdog"] = {Name = "Hotdog",InputShown = "",Tip = "Eat a hotdog, healing 15 HP at the cost of 10 stamina.",Cooldown = 15,Icon = "rbxassetid://134322360499381",DisplayName = "Hotdog"};
 ["Revolver"] = {Name = "Revolver",InputShown = "",Tip = "Shoot with your revolver stunning any killers hit for 2 seconds, you\'ll have to reload after. Having this ability makes you lose 20 max stamina though!",Cooldown = 15,Icon = "rbxassetid://107624957891469",DisplayName = "Revolver"};
 ["Taunt"] = {Name = "Taunt",InputShown = "",Tip = "Taunt the killer gaining a forcefield, highlighting the killer, and slowing them down for 5 seconds or until you\'re hit for the duration of the effect (1.25x damage).",Cooldown = 25,Icon = "rbxassetid://85436299122876",DisplayName = "Taunt"};
+["Banana"] = {Name = "Banana",InputShown = "",Tip = "Toss a banana onto the floor, if the killer or the civilian who made it touch the banana, they will slip and be briefly stunned, the banana will naturally decay over time.",Cooldown = 20,Icon = "rbxassetid://96202444819611",DisplayName = "Banana Peel"};
 
 --\\ Killers M1 //--
 ["Swing"] = {Name = "Swing",InputShown = "M1",Tip = "Swing forward to deal damage.",Cooldown = 1,Icon = "rbxassetid://13754070639",DisplayName = "Swing"}; -- Template
@@ -18,12 +19,12 @@ return {
 ["ArtfulSwing"] = {Name = "Swing",InputShown = "M1",Tip = "Swing forward to deal damage.",Cooldown = 1,Icon = "rbxassetid://95828395635772",DisplayName = "Swing"}; -- Fartful
 ["HarkenSwing"] = {Name = "Swing",InputShown = "M1",Tip = "Swing forward to deal damage.",Cooldown = 1,Icon = "rbxassetid://79789896606805",DisplayName = "Swing"}; -- Harken
 ["BadwareSwing"] = {Name = "Swing",InputShown = "M1",Tip = "Swing forward to deal damage.",Cooldown = 1,Icon = "rbxassetid://91900292604311",DisplayName = "Swing"}; -- Badware
-["KilldroidSwing"] = {Name = "Eject",InputShown = "M1",Tip = "Shoot a missile forward to deal damage.",Cooldown = 2.5,Icon = "rbxassetid://104399918505448",DisplayName = "Eject"}; -- Killdroid
+["Eject"] = {Name = "Eject",InputShown = "M1",Tip = "Shoot a missile forward to deal damage.",Cooldown = 2.5,Icon = "rbxassetid://104399918505448",DisplayName = "Eject"}; -- Killdroid
 
 --\\ Killer Abilities //--
 ["Cleave"] = {Name = "Cleave",InputShown = "",Tip = "Lunge forward and deal 20 damage and 10 bleed, long endlag and highlights the victim.",Cooldown = 20,Icon = "rbxassetid://92447235780730",DisplayName = "Cleave"};
 ["Howl"] = {Name = "Howl",InputShown = "",Tip = "Scream and slow everyone on the map for 6s.",Cooldown = 35,Icon = "rbxassetid://121485044324107",DisplayName = "Howl"};
-["Stalk"] = {Name = "Stalk",InputShown = "",Tip = "Turn invisible and gain a lot of speed, cant use abilities and a long endlag after.",Cooldown = 10,Icon = "rbxassetid://93106430986611",DisplayName = "Stalk"};
+["Stalk"] = {Name = "Stalk",InputShown = "",Tip = "Turn invisible and gain a lot of speed, cant use abilities and a long endlag after.",Cooldown = 10,Icon = "rbxassetid://92577246919936",DisplayName = "Stalk"};
 
 ["FirewallBypass"] = {Name = "FirewallBypass",InputShown = "",Tip = "Place a computer that speeds you up when your near it, +5 damage for the bolt ability and civilians can destroy them.",Cooldown = 15,Icon = "rbxassetid://128815994656979",DisplayName = "Firewall Bypass"};
 ["Bolt"] = {Name = "Bolt",InputShown = "",Tip = "Charge forward dealing a set amount of damage but knockbacks and ragdolls a civilian and you.",Cooldown = 20,Icon = "rbxassetid://72545932076875",DisplayName = "Bolt"};
@@ -31,349 +32,24 @@ return {
 
 ["Implement"] = {Name = "Implement",InputShown = "",Tip = "Place down a temporary wall, decays 1 HP per second and going through them damages it by 5.",Cooldown = 15,Icon = "rbxassetid://136267634030688",DisplayName = "Implement"};
 ["Copywrite"] = {Name = "Copywrite",InputShown = "",Tip = "Place down a music box that slows survivors and highlights them.",Cooldown = 35,Icon = "rbxassetid://135215798929697",DisplayName = "Copywrite"};
+["Repurpose"] = {Name = "Repurpose",InputShown = "",Tip = "Reach in front of you and ''Pocket'' whatever you hit, this move changes functionality upon hitting a Wall, Music Box, or Civilian.",Cooldown = 5,Icon = "rbxassetid://109187183475737",DisplayName = "Repurpose"};
 
 ["Flight"] = {Name = "Flight",InputShown = "",Tip = "Fly up high and gain aimbot for the duration, come back down after a while with endlag.",Cooldown = 20,Icon = "rbxassetid://119091263099069",DisplayName = "Flight"};
 ["Deploy"] = {Name = "Deploy",InputShown = "",Tip = "Places a killbot that if survivors come near, it can highlight and beep 5 times before exploding.",Cooldown = 15,Icon = "rbxassetid://96669704702337",DisplayName = "Deploy"};
+["Detonate"] = {Name = "Detonate",InputShown = "",Tip = "Stop in place and begin a count down, gain a massive speed boost, but lose the ability to regain stamina for its duration, upon the countdown finishing, explode dealing 25 damage in a large radius, deals self damage.",Cooldown = 35,Icon = "rbxassetid://132441884278500",DisplayName = "Detonate"};
 
 ["Repress"] = {Name = "Repress",InputShown = "",Tip = "Give every survivor a red light green light mini game which makes noise and blinds them, particles on them means it's active.",Cooldown = 60,Icon = "rbxassetid://119980024420991",DisplayName = "Repress"};
 ["Tangle"] = {Name = "Tangle",InputShown = "",Tip = "Throw a light spear that reels and heals the survivor, but any damage crits them and breaking the chain makes noise.",Cooldown = 25,Icon = "rbxassetid://136216951578398",DisplayName = "Tangle"};
 ["Immolate"] = {Name = "Immolate",InputShown = "",Tip = "Sacrifice 100 Health in total to add 15 noise to the meter and get a temporary speedboost.",Cooldown = 35,Icon = "rbxassetid://86152452436996",DisplayName = "Immolate"};
 
 --\\ Test/Unreleased/Scrapped/Admin Abilities //--
-["Untitled"] = {Name = "Untitled",InputShown = "?",Tip = "No tips for this ability.",Cooldown = 10,Icon = "rbxassetid://82116081649912",DisplayName = "Untitled"};
-["Template"] = {Name = "Template",InputShown = "",Tip = "This is template. Create your own abiltity!",Cooldown = 0,Icon = "rbxassetid://0",DisplayName = "Template"};
 ["Whack"] = {Name = "Whack",InputShown = "M1",Tip = "Swing forward to deal damage.",Cooldown = 0.5,Icon = "rbxassetid://13771861804",DisplayName = "Whack"};
-["Yield"] = {Name = "Yield",InputShown = "",Tip = "?",Cooldown = 20,Icon = "rbxassetid://82116081649912",DisplayName = "Yield"};
-["Bloodstung"] = {Name = "Bloodstung",InputShown = "",Tip = "?",Cooldown = 30,Icon = "rbxassetid://82116081649912",DisplayName = "Bloodstung"};
 ["Alter"] = {Name = "Alter",InputShown = "",Tip = "?",Cooldown = 30,Icon = "rbxassetid://82116081649912",DisplayName = "Alter"};
 ["Slam"] = {Name = "Slam",InputShown = "",Tip = "?",Cooldown = 15,Icon = "rbxassetid://82116081649912",DisplayName = "Slam"};
 ["Thunderstorm"] = {Name = "Thunderstorm",InputShown = "",Tip = "?",Cooldown = 15,Icon = "rbxassetid://82116081649912",DisplayName = "Thunderstorm"};
 ["SwordSwitch"] = {Name = "SwordSwitch",InputShown = "",Tip = "?",Cooldown = 10,Icon = "rbxassetid://82116081649912",DisplayName = "Sword Switch"};
+["Oil Drum"] = {Name = "Oil Drum",InputShown = "",Tip = "?",Cooldown = 10,Icon = "rbxassetid://82116081649912",DisplayName = "Oil Drum"};
+["Flamethrower"] = {Name = "Flamethrower",InputShown = "",Tip = "?",Cooldown = 10,Icon = "rbxassetid://82116081649912",DisplayName = "Flamethrower"};
+["Oil Spill"] = {Name = "Oil Spill",InputShown = "",Tip = "?",Cooldown = 10,Icon = "rbxassetid://82116081649912",DisplayName = "Oil Spill"};
 
 }
-
---[[
-
--- Decompiled by Krnl
-
-local v1 = {
-	["Swing"] = {
-		["Name"] = "Swing",
-		["EnergyCost"] = 0,
-		["Cooldown"] = 1,
-		["InputShown"] = "M1",
-		["DisplayName"] = "Swing",
-		["Icon"] = "rbxassetid://13754070639",
-		["PursuerIcon"] = "rbxassetid://86297288069487",
-		["ArtfulIcon"] = "rbxassetid://95828395635772",
-		["HarkenIcon"] = "rbxassetid://79789896606805",
-		["BadwareIcon"] = "rbxassetid://91900292604311",
-		["Tip"] = "Swing forwards to deal 20 damage."
-	},
-	["Cleave"] = {
-		["Name"] = "Cleave",
-		["EnergyCost"] = 0,
-		["Cooldown"] = 25,
-		["InputShown"] = "Q",
-		["DisplayName"] = "Cleave",
-		["Icon"] = "rbxassetid://92447235780730",
-		["Tip"] = "Pause before lunging forwards, dealing 20 damage directly and 10 damage in bleed, bleeding targets are indicated to you."
-	},
-	["Howl"] = {
-		["Name"] = "Howl",
-		["EnergyCost"] = 0,
-		["Cooldown"] = 35,
-		["InputShown"] = "E",
-		["DisplayName"] = "Howl",
-		["Icon"] = "rbxassetid://121485044324107",
-		["Tip"] = "Howl into the air, greatly slowing anyone around you for 6 seconds."
-	},
-	["Stalk"] = {
-		["Name"] = "Stalk",
-		["EnergyCost"] = 0,
-		["Cooldown"] = 10,
-		["InputShown"] = "R",
-		["DisplayName"] = "Stalk",
-		["Icon"] = "rbxassetid://92577246919936",
-		["Tip"] = "Turn completely invisible and gain a large speed boost, but are unable to use attacks or abilities. Recast to cancel early.",
-		["MultiUse"] = 2.5,
-		["MultiUseDisplayName"] = "Unshroud",
-		["MultiUseIcon"] = "rbxassetid://93106430986611"
-	},
-	["FirewallBypass"] = {
-		["Name"] = "FirewallBypass",
-		["EnergyCost"] = 0,
-		["Cooldown"] = 15,
-		["InputShown"] = "Q",
-		["DisplayName"] = "Firewall Bypass",
-		["Icon"] = "rbxassetid://128815994656979",
-		["Tip"] = "Place down a PC that gives you a speed buff when you are near it. Required in order to use Rift."
-	},
-	["Bolt"] = {
-		["Name"] = "Bolt",
-		["EnergyCost"] = 0,
-		["Cooldown"] = 20,
-		["InputShown"] = "E",
-		["DisplayName"] = "Bolt",
-		["Icon"] = "rbxassetid://72545932076875",
-		["Tip"] = "Pause before rushing forwards as a moving hitbox, dealing 10 damage, and an additional 5 for each PC you have."
-	},
-	["Rift"] = {
-		["Name"] = "Rift",
-		["EnergyCost"] = 0,
-		["Cooldown"] = 25,
-		["InputShown"] = "R",
-		["DisplayName"] = "Rift",
-		["Icon"] = "rbxassetid://127133544413220",
-		["Tip"] = "Stop in place and select a PC to teleport to it along with gaining a speed boost, but destroying the chosen PC."
-	},
-	["Implement"] = {
-		["Name"] = "Implement",
-		["EnergyCost"] = 0,
-		["Cooldown"] = 15,
-		["InputShown"] = "Q",
-		["DisplayName"] = "Implement",
-		["Icon"] = "rbxassetid://136267634030688",
-		["Tip"] = "Quickly create a wall that rises from the ground where you stand, slowly gets destroyed over time, but has more health the more civilians there are."
-	},
-	["Copywrite"] = {
-		["Name"] = "Copywrite",
-		["EnergyCost"] = 0,
-		["Cooldown"] = 35,
-		["InputShown"] = "E",
-		["DisplayName"] = "Copywrite",
-		["Icon"] = "rbxassetid://135215798929697",
-		["Tip"] = "Creates a Music Box at your current location that will greatly slow and indicate any civilian that enters its radius."
-	},
-	["Repurpose"] = {
-		["Name"] = "Repurpose",
-		["EnergyCost"] = 0,
-		["Cooldown"] = 5,
-		["InputShown"] = "R",
-		["DisplayName"] = "Repurpose",
-		["Icon"] = "rbxassetid://109187183475737",
-		["Tip"] = "Reach in front of you and \"Pocket\" whatever you hit, this move changes functionality upon hitting a Wall, Music Box, or Civilian."
-	},
-	["Eject"] = {
-		["Name"] = "Eject",
-		["EnergyCost"] = 0,
-		["Cooldown"] = 2.5,
-		["InputShown"] = "M1",
-		["DisplayName"] = "Eject",
-		["Icon"] = "rbxassetid://104399918505448",
-		["Tip"] = "Slow to a crawl, before firing a Missile forwards that deals 25 damage in a splash radius."
-	},
-	["Flight"] = {
-		["Name"] = "Flight",
-		["EnergyCost"] = 0,
-		["Cooldown"] = 20,
-		["InputShown"] = "Q",
-		["DisplayName"] = "Flight",
-		["Icon"] = "rbxassetid://119091263099069",
-		["Tip"] = "Pause in place before flying up into the air, Eject gains aimbot while you are not grounded, slowly lower to the ground over time."
-	},
-	["Deploy"] = {
-		["Name"] = "Deploy",
-		["EnergyCost"] = 0,
-		["Cooldown"] = 15,
-		["InputShown"] = "E",
-		["DisplayName"] = "Deploy",
-		["Icon"] = "rbxassetid://96669704702337",
-		["Tip"] = "Call in a Killbot at your current location that will indicate and damage Civilians if activated too many times."
-	},
-	["Detonate"] = {
-		["Name"] = "Detonate",
-		["EnergyCost"] = 0,
-		["Cooldown"] = 35,
-		["InputShown"] = "R",
-		["DisplayName"] = "Detonate",
-		["Icon"] = "rbxassetid://132441884278500",
-		["Tip"] = "Stop in place and begin a count down, gain a massive speed boost, but lose the ability to regain stamina for its duration, upon the countdown finishing, explode dealing 25 damage in a large radius, deals self damage."
-	},
-	["Yield"] = {
-		["Name"] = "Yield",
-		["EnergyCost"] = 0,
-		["Cooldown"] = 20,
-		["InputShown"] = "Q",
-		["DisplayName"] = "Yield",
-		["Icon"] = "rbxassetid://82116081649912",
-		["Tip"] = "?"
-	},
-	["Repress"] = {
-		["Name"] = "Repress",
-		["EnergyCost"] = 0,
-		["Cooldown"] = 60,
-		["InputShown"] = "Q",
-		["DisplayName"] = "Repress",
-		["Icon"] = "rbxassetid://119980024420991",
-		["Tip"] = "Force all survivors to play a minigame, failing to play along with the minigame blurs their screen, and awards you a chunk of your Noise Meter, Costs 15 Noise to use."
-	},
-	["Tangle"] = {
-		["Name"] = "Tangle",
-		["EnergyCost"] = 0,
-		["Cooldown"] = 25,
-		["InputShown"] = "E",
-		["DisplayName"] = "Tangle",
-		["Icon"] = "rbxassetid://136216951578398",
-		["Tip"] = "Toss a javelin forwards that will tether and drag the hit civilian to you, healing both of you, any damage delt to the tethered civilian will be boosted, generates Noise on use."
-	},
-	["Immolate"] = {
-		["Name"] = "Immolate",
-		["EnergyCost"] = 0,
-		["Cooldown"] = 35,
-		["InputShown"] = "R",
-		["DisplayName"] = "Immolate",
-		["Icon"] = "rbxassetid://86152452436996",
-		["Tip"] = "Consume 100 damage over time to fill a large majority of your Noise Meter, and grant yourself a speed boost."
-	},
-	["Slam"] = {
-		["Name"] = "Slam",
-		["EnergyCost"] = 0,
-		["Cooldown"] = 15,
-		["InputShown"] = "Q",
-		["DisplayName"] = "Slam",
-		["Icon"] = "rbxassetid://82116081649912",
-		["Tip"] = "?"
-	},
-	["Thunderstorm"] = {
-		["Name"] = "Thunderstorm",
-		["EnergyCost"] = 0,
-		["Cooldown"] = 15,
-		["InputShown"] = "E",
-		["DisplayName"] = "Thunderbolt",
-		["Icon"] = "rbxassetid://82116081649912",
-		["Tip"] = "?"
-	},
-	["SwordSwitch"] = {
-		["Name"] = "SwordSwitch",
-		["EnergyCost"] = 0,
-		["Cooldown"] = 10,
-		["InputShown"] = "R",
-		["DisplayName"] = "Sword Switch",
-		["Icon"] = "rbxassetid://82116081649912",
-		["Tip"] = "?"
-	},
-	["Oil Drum"] = {
-		["Name"] = "Oil Drum",
-		["EnergyCost"] = 0,
-		["Cooldown"] = 10,
-		["InputShown"] = "Q",
-		["DisplayName"] = "Oil Drum",
-		["Icon"] = "rbxassetid://82116081649912",
-		["Tip"] = "?"
-	},
-	["Flamethrower"] = {
-		["Name"] = "Flamethrower",
-		["EnergyCost"] = 0,
-		["Cooldown"] = 10,
-		["InputShown"] = "E",
-		["DisplayName"] = "Flamethrower",
-		["Icon"] = "rbxassetid://82116081649912",
-		["Tip"] = "?"
-	},
-	["Oil Spill"] = {
-		["Name"] = "Oil Spill",
-		["EnergyCost"] = 0,
-		["Cooldown"] = 10,
-		["InputShown"] = "R",
-		["DisplayName"] = "Oil Spill",
-		["Icon"] = "rbxassetid://82116081649912",
-		["Tip"] = "?"
-	},
-	["Adrenaline"] = {
-		["Name"] = "Adrenaline",
-		["Cooldown"] = 35,
-		["DisplayName"] = "Adrenaline",
-		["Icon"] = "rbxassetid://116399911657417",
-		["IsSurvivorAbility"] = true,
-		["Tip"] = "Gain a massive speed boost for 6 seconds, afterwards you will be stunned briefly, unable to move or get stamina, you will be highlighted to your teammates for its duration."
-	},
-	["Punch"] = {
-		["Name"] = "Punch",
-		["Cooldown"] = 40,
-		["DisplayName"] = "Punch",
-		["Icon"] = "rbxassetid://97428323453639",
-		["IsSurvivorAbility"] = true,
-		["Tip"] = "Slow down tremendously and punch forwards, striking a killer will stun them for 3 seconds, missing will briefly stun you instead."
-	},
-	["Caretaker"] = {
-		["Name"] = "Caretaker",
-		["Cooldown"] = 30,
-		["DisplayName"] = "Caretaker",
-		["Icon"] = "rbxassetid://90712805517714",
-		["IsSurvivorAbility"] = true,
-		["Tip"] = "Use a potion to heal anyone standing in front of you, healing them for 10 Health instantly, and another 10 over time. Having this ability lowers your max health by 25.",
-		["HealthModifier"] = -25,
-		["OneBounceDisabled"] = true
-	},
-	["Cloak"] = {
-		["Name"] = "Cloak",
-		["Cooldown"] = 50,
-		["DisplayName"] = "Cloak",
-		["Icon"] = "rbxassetid://90476367580326",
-		["IsSurvivorAbility"] = true,
-		["Tip"] = "Go entirely invisible, but lose the ability to regain stamina, along with become forced to use stamina to move, taking damage will force you out of Cloak early."
-	},
-	["Block"] = {
-		["Name"] = "Block",
-		["Cooldown"] = 40,
-		["DisplayName"] = "Block",
-		["Icon"] = "rbxassetid://120929805037270",
-		["IsSurvivorAbility"] = true,
-		["Tip"] = "Prepare yourself to Guard against an oncoming attack, a successful block negates damage and heal you for 10 health, prevent the killer from using attacks briefly, and gives you a speed boost, failing will stun you briefly."
-	},
-	["Dash"] = {
-		["Name"] = "Dash",
-		["Cooldown"] = 20,
-		["DisplayName"] = "Dash",
-		["Icon"] = "rbxassetid://73777691791017",
-		["IsSurvivorAbility"] = true,
-		["Tip"] = "Quickly Dash a good distance in the direction you are facing, but lose 25 stamina after using it."
-	},
-	["BonusPad"] = {
-		["Name"] = "BonusPad",
-		["Cooldown"] = 60,
-		["DisplayName"] = "BonusPad",
-		["Icon"] = "rbxassetid://86775625332300",
-		["IsSurvivorAbility"] = true,
-		["Tip"] = "Pause in place for a lengthy duration and build a Bonus Pad, anyone who steps on it is given a speed boost, however you lose 10 max health.",
-		["HealthModifier"] = -10
-	},
-	["Hotdog"] = {
-		["Name"] = "Hotdog",
-		["Cooldown"] = 15,
-		["DisplayName"] = "Hotdog",
-		["Icon"] = "rbxassetid://134322360499381",
-		["IsSurvivorAbility"] = true,
-		["Tip"] = "Your movement slows to a crawl as you consume a Hot Dog, you will regain 15 health, but lose 10 max stamina."
-	},
-	["Revolver"] = {
-		["Name"] = "Revolver",
-		["Cooldown"] = 15,
-		["DisplayName"] = "Revolver",
-		["Icon"] = "rbxassetid://107624957891469",
-		["IsSurvivorAbility"] = true,
-		["Tip"] = "Shoot your gun forwards, stunning the killer if it hits them 2 seconds, afterwards you will need to manually reload before you can shoot again, however having this ability lowers your health by 25, and your stamina by 20.",
-		["StaminaModifier"] = -20
-	},
-	["Taunt"] = {
-		["Name"] = "Taunt",
-		["Cooldown"] = 25,
-		["DisplayName"] = "Taunt",
-		["Icon"] = "rbxassetid://85436299122876",
-		["IsSurvivorAbility"] = true,
-		["Tip"] = "Taunt the killer at close range, successfully taunting a killer will cover their screen in distractions and indicate them globally to all civilians, also gives you a immunity against damage shortly."
-	},
-	["Banana"] = {
-		["Name"] = "Banana",
-		["Cooldown"] = 20,
-		["DisplayName"] = "Banana Peel",
-		["Icon"] = "rbxassetid://96202444819611",
-		["IsSurvivorAbility"] = true,
-		["Tip"] = "Toss a banana onto the floor, if the killer or the civilian who made it touch the banana, they will slip and be briefly stunned, the banana will naturally decay over time."
-	}
-}
-Module = v1
-return Module
-
-]]--
